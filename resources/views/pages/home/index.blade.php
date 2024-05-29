ff77be905219159701d21509b050ef03059f3b81
Juandach7 <juanda07112020@gmail.com>
2024-05-29 08:50:57 -0500

    Changes Alert

--- resources/views/pages/home/index.blade.php
+++ resources/views/pages/home/index.blade.php
@@ -1,2 +1,18 @@
-
+@section('scripts')
+    <script>
+        Swal.fire({
+  title: "<strong>ADVERTENCIA</strong>",
+  icon: "warning",
+  html: `
+    Entrega <b>tardia</b>,
+    de las llaves
+    `,
+  focusConfirm: false,
+  confirmButtonText: `
+    <i class="fa fa-thumbs-up"></i> Aceptar!
+  `,
+  confirmButtonAriaLabel: "Thumbs up, great!",
+});
+    </script>
+@endsection
 
